bce63060e75ef533be9e578512d268e9eea06e20
mattgpaul <mattgpaul@gmail.com>
2022-07-29 14:46:12 -0600

    ready to define the model functions

--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
 import sys
 import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
 
 sys.path.append('..')
 
 
-def Normalize(df, axis=1):
+def Normalize(df, axis=0):
     """Normalize a dataframe along an axis.
 
     Args:
         df (pd.DataFrame): Pandas dataframe of size mxn.
-        axis (int, optional): axis to perform statistics operations. Defaults to 1.
+        axis (int, optional): axis to perform statistics operations. Defaults to 0.
 
     Returns:
         df_norm (pd.DataFrame): Returns a dataframe with the same size as the input. Dataframe values are now formated as (df - mean)/std for each entry along the user specified axis
@@ -22,3 +24,23 @@ def Normalize(df, axis=1):
     df_norm = (df - df_mu)/df_sigma
 
     return df_norm, df_mu, df_sigma
+
+
+def CreateFeatureColumns(df, features):
+    """Creates tensorflow feature columns
+
+    Args:
+        features (list): columns in a dataframe that need to be transferred to feature columns
+
+    Returns:
+        feature_columns (tensorflow): numeric feature columns from the dataframe
+    """
+
+    feature_columns = []
+    for feature in features:
+        feature_columns.append(tf.feature_column.numeric_column(feature))
+
+    feature_layer = layers.DenseFeatures(feature_columns)
+    feature_layer(dict(df))
+
+    return feature_layer
